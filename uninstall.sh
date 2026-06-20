021cee8ae3a2d89659d38175ccfb3e9d7ee6a100
Lanoschachy <141957332+Lanoschachy@users.noreply.github.com>
2026-06-21 02:50:02 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+echo "Removing Padbury Linux..."
+
+pkill xautolock
+pkill -f padbury.py
+
+rm -rf ~/.padbury
+
+rm -f ~/.config/autostart/padbury.desktop
+
+rm -f ~/.local/share/fonts/Montserrat-ExtraLight.ttf
+
+fc-cache -fv > /dev/null
+
+echo ""
+echo "================================="
+echo "Padbury Linux removed!"
+echo "================================="
